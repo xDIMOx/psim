0d90bed93bfb99f2f4050d9ae3c0b53f3e7d4b10
dimo <dalmonimo@gmail.com>
2020-04-13 19:37:11 -0300

    datapath: error handling

--- datapath.c
+++ datapath.c
@@ -10,10 +10,36 @@
 #include "cpu.h"
 #include "mem.h"
 
+/* Implements */
+#include "datapath.h"
+
+#define X(a, b) b,
+static const char *Datapath_errlist[] = {
+	DatapathErrList
+};
+#undef X
+
 int             Datapath_execute(CPU *cpu, Mem *mem);
+
+const char     *Datapath_strerror(int errno);
 
 int
 Datapath_execute(CPU *cpu, Mem *mem)
 {
+	Datapath_errno = DATAPATHERR_SUCC;
+
 	return 0;
+}
+
+/*
+ * Datapath_strerror: Map error number to error message string
+ *
+ * errno: error number
+ *
+ * Returns error message string
+ */
+inline const char *
+Datapath_strerror(int errno)
+{
+	return Datapath_errlist[errno];
 }
--- datapath.h
+++ datapath.h
@@ -23,5 +23,21 @@ typedef struct {
 	uint32_t        sel;	/* coprocessor select */
 	uint32_t        cofun;	/* coprocessor operation */
 } Decoder;
+/*
+ * Error handling
+ */
+
+#define DatapathErrList \
+X(DATAPATHERR_SUCC, "Success")
+
+#define X(a, b) a,
+enum DatapathErrNo {
+	DatapathErrList
+};
+#undef X
+
+int             Datapath_errno;
 
 int             Datapath_execute(CPU *cpu, Mem *mem);
+
+const char     *Datapath_strerror(int errno);
