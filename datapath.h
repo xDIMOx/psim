e0efda53a30cbd4c58104486bdc063fbd173039c
dimo <dalmonimo@gmail.com>
2020-04-13 19:37:34 -0300

    datapath: remove decoder structure

--- datapath.h
+++ datapath.h
@@ -4,25 +4,6 @@
  * Processor's datapath
  */
 
-/* Instruction decoder */
-typedef struct {
-	uint32_t        raw;
-	uint32_t        sign;	/* instruction signature */
-	const char     *name;
-	int32_t         isjump;	/* instruction is a jump */
-	uint32_t        pc;
-	uint32_t        npc;
-	uint8_t         rd;
-	uint8_t         rs, rt;
-	uint8_t         sa;
-	int16_t         imm;
-	int16_t         ismem;	/* the intruction uses the memory */
-	uint32_t        idx;
-	uint32_t        region;
-	uint32_t        addr;	/* address to be acessed */
-	uint32_t        sel;	/* coprocessor select */
-	uint32_t        cofun;	/* coprocessor operation */
-} Decoder;
 /*
  * Error handling
  */
