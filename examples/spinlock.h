4938029e8cc94d1b662b15be40fe89180483f4e4
dimo <dalmonimo@gmail.com>
2020-04-30 14:04:58 -0300

    examples: implement spinlock functions

---
+++ examples/spinlock.h
@@ -0,0 +1,8 @@
+/* Check LICENSE file for copyright and license details. */
+
+/*
+ * Spinlock functions
+ */
+
+void            Spin_lock(int *l);
+void            Spin_unlock(int *l);
---
+++ examples/spinlock.s
@@ -0,0 +1,35 @@
+# Check LICENSE file for copyright and license details.
+
+	.text
+	.align 0
+	.set noreorder
+	.global Spin_lock
+	.ent Spin_lock
+Spin_lock:
+	ll $t0, 0($a0)
+	addi $t0, $t0, -1
+	bltz $t0, Spin_lock
+	nop
+	sc $t0, 0($a0)
+	beqz $t0, Spin_lock
+	nop
+	jr $ra
+	nop
+
+	.end Spin_lock
+
+	.text
+	.align 0
+	.set noreorder
+	.global Spin_unlock
+	.ent Spin_unlock
+Spin_unlock:
+	ll $t0, 0($a0)
+	addiu $t0, $t0, 1
+	sc $t0, 0($a0)
+	beqz $t0, Spin_unlock
+	nop
+	jr $ra
+	nop
+
+	.end Spin_unlock
