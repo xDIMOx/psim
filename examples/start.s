6efed9caec608adbcc01877f65bf68cba9e407d9
dimo <dalmonimo@gmail.com>
2020-05-25 14:57:56 -0300

    examples: set stack frame correctly

--- examples/start.s
+++ examples/start.s
@@ -7,6 +7,14 @@
 	.global _start
 	.ent _start
 _start:
+	# set stack frame
+	c2 0 # get memory size on $k1
+	addiu $t0, $zero, STKSZ
+	mul $t0, $t0, $k0
+	subu $sp, $k1, $t0
+	addiu $sp, $sp, -4
+
+	# start code
 	jal main
 	nop
 
@@ -16,3 +24,8 @@ BUSYWAIT:
 	nop
 
 	.end _start
+
+	.bss
+	.align 0
+
+	.equ STKSZ,(1 << 13)
