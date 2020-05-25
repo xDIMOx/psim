6d0e5d24a298bfb5be3f93639638739445605006
dimo <dalmonimo@gmail.com>
2020-05-25 14:58:30 -0300

    examples: do not use the gp register

--- examples/config.mk
+++ examples/config.mk
@@ -6,8 +6,8 @@ AS     = mipsel-elf-as
 AFLAGS = -Os -mips32r2 -msoft-float
 
 CC     = mipsel-elf-gcc
-CFLAGS = -Os -nostartfiles -mips32r2 -msoft-float -fno-builtin -mno-memcpy \
-         -fno-stack-protector -fno-delayed-branch
+CFLAGS = -O1 -nostartfiles -mips32r2 -msoft-float -fno-builtin -mno-memcpy \
+         -fno-stack-protector -fno-delayed-branch -mno-gpopt
 
 LD      = mipsel-elf-ld
 LDFLAGS = -static
