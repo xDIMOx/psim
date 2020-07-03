2e6810cfe52b674fcd8201337b4ad1eae438c90d
dimo <dalmonimo@gmail.com>
2020-07-03 18:24:58 -0300

    misc: set NDEBUG macro

--- config.mk
+++ config.mk
@@ -6,5 +6,5 @@ CFLAGS  = -std=c99 -Wall -Wpedantic -pedantic-errors -Wextra -Wfatal-errors \
           -Wformat=2 -Wlogical-op -Wjump-misses-init -Wshadow \
           -Wpadded -Wredundant-decls -Wcast-qual -Wcast-align \
           -Wstrict-overflow=5 -Wno-redundant-decls -fno-builtin \
-          -Os -D_XOPEN_SOURCE=700
+          -Os -D_XOPEN_SOURCE=700 -DNDEBUG
 LDFLAGS = -s -static -lpthread
