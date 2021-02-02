75bb439f6c729698d327864a6fe64ee73e30c578
dimo <dalmonimo@gmail.com>
2021-02-02 13:58:32 -0300

    examples: improve the random number generation

--- examples/src/libcommon/randu.s
+++ examples/src/libcommon/randu.s
@@ -4,24 +4,22 @@
 	.align 0
 	.set noreorder
 	.global randu
-	.extern Spin_lock
-	.extern Spin_unlock
 	.ent randu
 randu:
 	addiu $sp, $sp, -4
 	sw $ra, 0($sp)
-	la $a0, randulock
-	jal Spin_lock
 	nop
 	la $t0, randuseed
-	lw $t1, 0($t0)
+TRY:
+	ll $t1, 0($t0)
 	la $t2, 0x10003
 	mul $t1, $t1, $t2
-	sw $t1, 0($t0)
+	move $t3, $t1
+	sc $t3, 0($t0)
+	beqz $t3, TRY
+	nop
 	la $t2, 0x7fffffff
 	and $v0, $t1, $t2
-	jal Spin_unlock
-	nop
 	lw $ra, 0($sp)
 	addiu $sp, $sp, 4
 	jr $ra
